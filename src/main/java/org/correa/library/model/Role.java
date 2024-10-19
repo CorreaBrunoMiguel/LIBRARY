41a41b692a5018d84eb4eae85a896a8fb455c611
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-10-19 15:36:30 -0300

    criado entity Role

---
+++ src/main/java/org/correa/library/model/Role.java
@@ -0,0 +1,24 @@
+package org.correa.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+}
