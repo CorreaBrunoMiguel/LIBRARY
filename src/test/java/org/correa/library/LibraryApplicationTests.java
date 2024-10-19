675de58137a925c5f68c6bbc0c630672876d8cf8
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-10-19 13:59:56 -0300

    Init

---
+++ src/test/java/org/correa/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.correa.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
