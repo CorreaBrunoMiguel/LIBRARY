12940608f21efb32284b59f0a1d626a6258d5e83
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-10-19 15:34:15 -0300

    criado entity User

---
+++ src/main/java/org/correa/library/model/User.java
@@ -0,0 +1,41 @@
+package org.correa.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long Id;
+
+    private String username;
+    private String password;
+    private String numeroCelular;
+
+    @ManyToMany(fetch = EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    @OneToMany(mappedBy = "user")
+    private List<Emprestimo> emprestimos;
+}
